0b88e5d270de29faa23970940e6d7496217a8440
Chinna5656 <chinnakit547@gmail.com>
2026-06-10 17:00:50 +0700

    nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,20 @@
-import type { NextConfig } from "next";
+/*import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
+
+export default nextConfig;
+*/
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "g9szrrl7-3000.asse.devtunnels.ms",
+        "localhost:3000",
+      ],
+    },
+  },
+};
 
 export default nextConfig;
